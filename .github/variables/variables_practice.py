73240ff1fd488e99227e37c795d2d27c878d1407
Alan Andreoni <90458204+Grimlyy@users.noreply.github.com>
2021-10-29 09:22:08 -0400

    Version 0.1
    added account balance ad health

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,8 @@
 # Variables Practice, Alan Andreoni, 10/29/21 9:05 v0.0
+
+# Three Main Types Of Variables for this class
+
+health = 64128 # INTEGERS, any +/- whole number and 0
+account_balance = 36178391.00 # FLOATING POINT, any +/- with decimal.
+
+
